4dda24b18a4df0b0ce7f73873c9968fcb7dd7730
briandridge <briandridge@gmail.com>
2018-11-16 23:23:42 -0700

    cleaned up

--- src/App.js
+++ src/App.js
@@ -5,39 +5,58 @@ import {
   Subtitle,
   Introduction,
   Contact,
+  ContactWrapper,
+  Link,
+  Icon,
 } from './App.styled';
 
 class App extends Component {
   render() {
     return <AppWrapper>
         <Title>Brian D. Ridge</Title>
-        <Subtitle>Front-end web development</Subtitle>
+        <Subtitle>Front-end software engineer</Subtitle>
         <Introduction>
-          I focus on an effortless user experience when building performant, accessable, beautiful UIs. I focus on JavaScript, React, and styled-components.
+          I focus on an effortless user experience when building performant,
+          accessable, beautiful UIs. I focus on JavaScript, React, and
+          styled-components.
         </Introduction>
-        <Contact>Get in touch with me here</Contact>
-        <div>briandridge@gmail.com</div>
-        <div>
-          <a href="./assets/images/photos/BrianDRidge.pdf" target="_blank">
-            Resume
-          </a>
-        </div>
-        <div>
-          <a href="http://www.linkedin.com/in/briandridge" target="_blank">
-            LinkedIn
-          </a>
-        </div>
-        <div>
-          <a href="http://twitter.com/briandridge" target="_blank">
-            @briandridge
-          </a>
-        </div>
-        <div>
-          <a href="http://github.com/briandridge" target="_blank">
-            Github
-          </a>
-        </div>
-        <div>My most recent project:</div>
+        <Introduction>
+          I'm currently working on a micro-front end and building a component library. Ask me about it :) 
+        </Introduction>
+        <Introduction>
+          Something I built for fun: 
+          <Link href="http://www.sombrerorecords.com" > Sombrero Records </Link>
+        </Introduction>
+        <ContactWrapper>
+          <Contact>
+            <Icon className="far fa-envelope" />
+            <span>briandridge@gmail.com</span>
+          </Contact>
+          <Contact>
+            <Link href="./assets/images/photos/BrianDRidge.pdf">
+              <Icon className="far fa-file" />
+              <span>Resume</span>
+            </Link>
+          </Contact>
+          <Contact>
+            <Link href="http://www.linkedin.com/in/briandridge">
+              <Icon className="fab fa-linkedin" />
+              <span>linkedin.com/in/briandridge</span>
+            </Link>
+          </Contact>
+          <Contact>
+            <Link href="http://twitter.com/briandridge">
+              <Icon className="fab fa-twitter" />
+              <span>@briandridge</span>
+            </Link>
+          </Contact>
+          <Contact>
+            <Link href="http://github.com/briandridge">
+              <Icon className="fab fa-github" />
+              <span>Github</span>
+            </Link>
+          </Contact>
+        </ContactWrapper>
       </AppWrapper>;
   }
 }
--- src/App.styled.js
+++ src/App.styled.js
@@ -1,26 +1,61 @@
 import styled from 'styled-components';
 
 const AppWrapper = styled.div`
-  text-align: center;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
   font-family: Raleway;
+  margin: 0 1rem;
 `;
 
 const Title = styled.div`
+  margin-top: 2rem;
   font-size: 2rem;
   font-weight: 700;
+  text-align: center;
 `;
 
 const Subtitle = styled.div`
+  margin-top: 1rem;
   font-size: 1.25rem;
   font-weight: 500;
+  text-align: center;
 `;
 
 const Introduction = styled.div`
+  margin: 1rem auto;
+  max-width: 500px;
   font-size: 1rem;
+  text-align: left;
+`;
+
+const ContactWrapper = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
 `;
 
 const Contact = styled.div`
-  font-size: 1rem;
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%;
+  height: 40px;
+  justify-content: start;
+  align-items: center;
+  background-color: lightgray;
+  border-top: 5px solid white;
+  font-weight: 600;
+  padding: 0 10px;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: black;
+`;
+
+const Icon = styled.span`
+  margin-right: 10px;
 `;
 
 export {
@@ -29,4 +64,7 @@ export {
   Subtitle,
   Introduction,
   Contact,
+  ContactWrapper,
+  Link,
+  Icon,
 };
